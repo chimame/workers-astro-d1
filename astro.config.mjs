0c362004d68b7af17032408c42c7eeb70b57dbc9
chimame <rito.tamata@gmail.com>
2023-01-12 02:10:15 +0900

    pnpm astro add react && pnpm astro add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  integrations: [react()],
+  adapter: cloudflare({ mode: "directory" })
+});
