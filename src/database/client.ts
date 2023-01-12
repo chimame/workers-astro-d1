40eeb78f7a2b9b48f59a4890270c1168fe4e3229
chimame <rito.tamata@gmail.com>
2023-01-12 18:50:45 +0900

    Add data fetch from D1

--- src/database/client.ts
+++ src/database/client.ts
@@ -3,4 +3,4 @@ import type { DB } from 'kysely-codegen'
 import { Kysely } from 'kysely'
 import { D1Dialect } from 'kysely-d1'
 
-export const client = new Kysely<DB>({ dialect: new D1Dialect({ database: import.meta.env.DB }) })
+export const client = (database: any) => new Kysely<DB>({ dialect: new D1Dialect({ database }) })
